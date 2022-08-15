ad6ea281221692601a4a2e6efbd0d5c02f38b9e7
IbrahimJebreen <ibrahim.jebreen1999@gmail.com>
2022-08-15 21:18:40 +0300

    create the table of the employees

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Employees program if the employee’s salary level is junior high between 1500-2000 and if his level is mid-senior 1000-1500 if his junior level is 500-1000
+In the render function :
+ You have to create the table of the employees and render it in the main of the home.html 
+
 
 [Live URL](https://ibrahimjebreen.github.io/management-system/)]
--- app.js
+++ app.js
@@ -27,14 +27,61 @@ let emp3 = new employee(14, "ahmad", "development", "Junior");
 let emp4 = new employee(15, "abdullah khaldon", "development", "Mid-Senior");
 let emp5 = new employee(16, "malek adel", "development", "Junior");
 let emp6 = new employee(17, "jehad haded", "development", "Senior");
+let emp7 = new employee(18, "ahmad al3krb", "development", "Senior");
 
+const main = document.querySelector("main");
+const table = document.createElement("table");
+main.appendChild(table);
 
-let employees = [emp1, emp2, emp3, emp4, emp5, emp6];
+const tr = document.createElement("tr");
+table.appendChild(tr);
+const th1 = document.createElement("td");
+tr.appendChild(th1);
+th1.textContent = "EmployeeID";
+const th2 = document.createElement("td");
+tr.appendChild(th2);
+th2.textContent = "Full Name";
+const th3 = document.createElement("td");
+tr.appendChild(th3);
+th3.textContent = "Department";
+const th4 = document.createElement("td");
+tr.appendChild(th4);
+th4.textContent = "Level";
+const th5 = document.createElement("td");
+tr.appendChild(th5);
+th5.textContent = "Salary";
+
+let employees = [emp1, emp2, emp3, emp4, emp5, emp6,emp7];
 function render() {
-    for (let i = 0; i < employees.length; i++) {
-
-        console.log(employees[i].fullName+" "+employees[i].salary);
+    for (let i = 0; i < 6; i++) {
+        console.log(employees[i].fullName + " " + employees[i].salary);
     }
 }
 
-render();
+// render();
+
+
+
+for (let row = 0; row < 7; row++) {
+    const tr = document.createElement("tr");
+    table.appendChild(tr);
+        const td1 = document.createElement("td");
+        tr.appendChild(td1);
+        td1.textContent = employees[row].employeeID;
+        const td2 = document.createElement("td");
+        tr.appendChild(td2);
+        td2.textContent = employees[row].fullName;
+        const td3 = document.createElement("td");
+        tr.appendChild(td3);
+        td3.textContent = employees[row].department;
+        const td4 = document.createElement("td");
+        tr.appendChild(td4);
+        td4.textContent = employees[row].level;
+        const td5 = document.createElement("td");
+        tr.appendChild(td5);
+        td5.textContent = employees[row].salary;
+}
+
+
+
+
