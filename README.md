1b53bc064c8377f33f65ec4bce73019dda1a5791
IbrahimJebreen <ibrahim.jebreen1999@gmail.com>
2022-08-15 15:48:56 +0300

    constructor

---
+++ README.md
@@ -0,0 +1,2 @@
+Employees program if the employee’s salary level is junior high between 1500-2000 and if his level is mid-senior 1000-1500 if his junior level is 500-1000
+
---
+++ app.js
@@ -0,0 +1,40 @@
+function employee(employeeID, fullName, department, level) {
+    this.employeeID = employeeID;
+    this.fullName = fullName;
+    this.department = department;
+    this.level = level;
+    this.salary=calcSalary(level);
+}
+
+    function calcSalary(level){
+        let salary=0;
+        if(level=="Senior"){
+            salary= Math.floor(Math.random() * 501) + 1500;
+        }
+        else if(level=="Mid-Senior"){
+            salary= Math.floor(Math.random() * 501) + 1000;
+        }
+        else if(level=="Junior"){
+            salary=Math.floor(Math.random() * 501) + 500;
+        }
+        return salary;
+    }
+
+
+let emp1=new employee(12,"ibrahim","development","Senior");
+let emp2=new employee(13,"mohamed","full stack","Mid-Senior");
+let emp3=new employee(14,"ahmad","development","Junior");
+let emp4=new employee(15,"abdullah khaldon","development","Mid-Senior");
+let emp5=new employee(16,"malek adel","development","Junior");
+let emp6=new employee(17,"jehad haded","development","Senior");
+
+
+let employees=[emp1,emp2,emp3,emp4,emp5,emp6];
+
+for(let i=0;i<employees.length;i++){
+
+    console.log(employees[i]);
+}
+
+
+
