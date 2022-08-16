574de7d6f1c2bf19fdd70c45ea6c7338717e6ee2
IbrahimJebreen <ibrahim.jebreen1999@gmail.com>
2022-08-16 18:53:13 +0300

    add event listener

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-function employee(employeeID, fullName, department, level) {
+function employee(employeeID, fullName, department, level, image) {
     this.employeeID = employeeID;
     this.fullName = fullName;
     this.department = department;
     this.level = level;
     this.salary = calcSalary(level);
+    this.image = image;
 }
 
 function calcSalary(level) {
@@ -51,37 +52,78 @@ const th5 = document.createElement("td");
 tr.appendChild(th5);
 th5.textContent = "Salary";
 
-let employees = [emp1, emp2, emp3, emp4, emp5, emp6,emp7];
-function render() {
-    for (let i = 0; i < 6; i++) {
-        console.log(employees[i].fullName + " " + employees[i].salary);
-    }
-}
+// let employees = [emp1, emp2, emp3, emp4, emp5, emp6, emp7];
+// function render() {
+//     for (let i = 0; i < 6; i++) {
+//         console.log(employees[i].fullName + " " + employees[i].salary);
+//     }
+// }
+
+// render();
+
+
+// function render() {
+//     for (let row = 0; row < 7; row++) {
+//         const tr = document.createElement("tr");
+//         table.appendChild(tr);
+//         const td1 = document.createElement("td");
+//         tr.appendChild(td1);
+//         td1.textContent = document.getElementById("empid").value;
+//         const td2 = document.createElement("td");
+//         tr.appendChild(td2);
+//         td2.textContent = employees[row].fullName;
+//         const td3 = document.createElement("td");
+//         tr.appendChild(td3);
+//         td3.textContent = employees[row].department;
+//         const td4 = document.createElement("td");
+//         tr.appendChild(td4);
+//         td4.textContent = employees[row].level;
+//         const td5 = document.createElement("td");
+//         tr.appendChild(td5);
+//         td5.textContent = employees[row].salary;
+//     }
+// }
 
 // render();
 
 
 
-for (let row = 0; row < 7; row++) {
+
+let form =document.getElementById("form");
+form.addEventListener('submit',function(event){
+    event.preventDefault();
     const tr = document.createElement("tr");
-    table.appendChild(tr);
+        table.appendChild(tr);
         const td1 = document.createElement("td");
         tr.appendChild(td1);
-        td1.textContent = employees[row].employeeID;
+        td1.textContent = document.getElementById("empid").value;
         const td2 = document.createElement("td");
         tr.appendChild(td2);
-        td2.textContent = employees[row].fullName;
+        td2.textContent = document.getElementById("fname").value;
         const td3 = document.createElement("td");
         tr.appendChild(td3);
-        td3.textContent = employees[row].department;
+        td3.textContent = document.getElementById("department").value;
         const td4 = document.createElement("td");
         tr.appendChild(td4);
-        td4.textContent = employees[row].level;
-        const td5 = document.createElement("td");
+        let x=td4.textContent = document.getElementById("level").value;
+        const td5 =document.createElement("td");
         tr.appendChild(td5);
-        td5.textContent = employees[row].salary;
-}
+        td5.textContent =calcSalary(document.getElementById("level").value);
+        
+        
+        
+        function calcSalary(level) {
+            let salary = 0;
+            if (String(level) == "Senior") {
+                salary = Math.floor(Math.random() * 501) + 1500;
+            }
+            else if (level == "Mid-Senior") {
+                salary = Math.floor(Math.random() * 501) + 1000;
+            }
+            else if (level == "Junior") {
+                salary = Math.floor(Math.random() * 501) + 500;
+            }
+            return salary;
+        };
 
-
-
-
+})
